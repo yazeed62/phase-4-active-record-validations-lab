fda019502506473a14701ccf57ac6444ec6bf279
Yazeed <dee3ayahmed25@gmail.com>
2023-07-15 07:01:57 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates : title, presence: true
+  validates :content, length { minimum: 250}
+  validates :summary, length { maximum: 250}
+   validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+   validate :sufficiently_clickbait_title
+
+  private
+
+  def sufficiently_clickbait_title
+    clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+    unless clickbait_phrases.include?(title)
+      errors.add(:title, 'should be a clickbait phrase')
+    end
+  end
 end
